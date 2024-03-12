b0095e14d7e4020f0c2d9a5b4172a87eec223f35
Malek Sharabi <m.malek1996@hotmail.com>
2024-03-12 07:43:33 +0100

    Add dynamic year to footer

--- 293-keeper-app-part-1-challenge-start/293-keeper-app-part-1-challenge/src/components/PFooter.jsx
+++ 293-keeper-app-part-1-challenge-start/293-keeper-app-part-1-challenge/src/components/PFooter.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+console.log(currentYear);
 
 function PFooter(){
-    return(<footer>Copyright ⓒ 2024</footer>)
+    return(<footer>Copyright ⓒ {currentYear}</footer>)
 }
 
 export default PFooter;
